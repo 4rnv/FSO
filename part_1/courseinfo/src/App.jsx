97dea445ff9c55344c37b476bf2672203d213e92
4rnv <arnavsdandekar@gmail.com>
2024-12-01 00:48:23 +0530

    Ex. 1dot3-1dot5

--- part_1/courseinfo/src/App.jsx
+++ part_1/courseinfo/src/App.jsx
@@ -1,6 +1,7 @@
-const Header = (course_name) => {
+const Header = (props) => {
+  console.log(props);
   return (
-    <h1>{course_name.course_name}</h1>
+    <h1>{props.course_name}</h1>
   )
 }
 
@@ -14,33 +15,34 @@ const Content = (props) => {
   console.log(props)
   return (
   <div>
-    <Part part={props.courses[0].part_name} exercises={props.courses[0].exercises} />
-    <Part part={props.courses[1].part_name} exercises={props.courses[1].exercises} />
-    <Part part={props.courses[2].part_name} exercises={props.courses[2].exercises} />
+    <Part part={props.course['parts'][0].part_name} exercises={props.course['parts'][0].exercises} />
+    <Part part={props.course['parts'][1].part_name} exercises={props.course['parts'][1].exercises} />
+    <Part part={props.course['parts'][2].part_name} exercises={props.course['parts'][2].exercises} />
   </div>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.course['parts'][0].exercises+props.course['parts'][1].exercises+props.course['parts'][2].exercises}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  
-  const course_array = [
-    {part_name: "Fundamentals of React", exercises: 10},
-    {part_name: "Using props to pass data", exercises: 7},
-    {part_name: "State of a component", exercises: 14}
-  ]
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {part_name: "Fundamentals of React", exercises: 10},
+      {part_name: "Using props to pass data", exercises: 7},
+      {part_name: "State of a component", exercises: 14}
+    ]
+  }
 
   return (
     <div>
-      <Header course_name={course}/>
-      <Content courses={course_array}/>
-      <Total exercises1={course_array[0].exercises}  exercises2={course_array[1].exercises}  exercises3={course_array[2].exercises}/>
+      <Header course_name={course.name}/>
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
 }
