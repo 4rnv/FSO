ad913f7f6cd321dff2a35b59fdbe8311ec884cd2
4rnv <arnavsdandekar@gmail.com>
2024-12-01 19:57:44 +0530

    Ex. 1dot8-1dot9

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -1,26 +1,52 @@
 import { useState } from 'react'
 
+const Statistics = ({good,neutral,bad}) => {
+  
+  if(good+neutral+bad===0) {
+    return (
+      <>
+      <h1>Statistics</h1>
+      <p>No feedback given</p>
+      </>
+    )
+  }
+  const calcAverage = (good,neutral,bad) => {
+    const average = (good*1 - bad*1)/(good+neutral+bad)
+    return average
+  }
+
+  return (
+    <>
+    <h1>Statistics</h1>
+    <p>Good: {good}</p>
+    <p>Neutral: {neutral}</p>
+    <p>Bad: {bad}</p>
+    <p>All: {good+neutral+bad}</p>
+    <p>Average: {calcAverage(good,neutral,bad)}</p>
+    <p>Positive Percentage: {good*100/(good+neutral+bad)}%</p>
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   const incrementGood = () => {
-    setGood(good+1)
-    console.log('New Value Good',good)
+    const newGood = good+1
+    setGood(newGood)
+    console.log('New Value Good',newGood)
   }
   const incrementNeutral = () => {
-      setNeutral(neutral+1)
-      console.log('New Value Neutral',neutral)
+    const newNeutral = neutral+1
+    setNeutral(newNeutral)
+    console.log('New Value Neutral',newNeutral)
   }
   const incrementBad = () => {
-      setBad(bad+1)
-      console.log('New Value Bad',bad)
-  }
-
-  const calcAverage = (good,neutral,bad) => {
-    const average = (good*1 - bad*1)/(good+neutral+bad)
-    return average
+    const newBad = bad+1
+    setBad(newBad)
+    console.log('New Value Bad',newBad)
   }
 
   return (
@@ -29,13 +55,7 @@ const App = () => {
       <button onClick={incrementGood}>Good</button>
       <button onClick={incrementNeutral}>Neutral</button>
       <button onClick={incrementBad}>Bad</button>
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {good+neutral+bad}</p>
-      <p>Average: {calcAverage(good,neutral,bad)}</p>
-      <p>Positive Percentage: {good*100/(good+neutral+bad)}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
