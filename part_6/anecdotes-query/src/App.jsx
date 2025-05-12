6e0de1f9a25d16cfb43ab929cb5538bdd88014d7
4rnv <arnavsdandekar@gmail.com>
2025-05-12 13:40:13 +0530

    Ex. 6dot23-6dot24

--- part_6/anecdotes-query/src/App.jsx
+++ part_6/anecdotes-query/src/App.jsx
@@ -2,9 +2,11 @@ import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import { getAnecdotes, createAnecdote, updateAnecdote } from './requests'
 import { useQueryClient, useQuery, useMutation } from '@tanstack/react-query'
+import { useNotificationDispatch } from './NotificationContext'
 
 const App = () => {
   const queryClient = useQueryClient()
+  const dispatch = useNotificationDispatch()
 
   const result = useQuery({
     queryKey: ['anecdotes'],
@@ -20,21 +22,24 @@ const App = () => {
 
   const handleVote = (anecdote) => {
     console.log('vote')
+    dispatch({
+      type: 'SHOW',
+      payload: `Voted on ${anecdote.id}.`
+    })
+    setTimeout(() => { dispatch({ type: 'HIDE' }) }, 3000)
     updateMutants.mutate({ ...anecdote, votes: anecdote.votes + 1 })
   }
 
-  if ( result.isLoading ) {
+  if (result.isLoading) {
     return <div>loading data...</div>
   }
   const anecdotes = result.data
-  
+
   return (
     <div>
       <h3>Anecdote app</h3>
-    
       <Notification />
       <AnecdoteForm />
-    
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
--- part_6/anecdotes-query/src/components/AnecdoteForm.jsx
+++ part_6/anecdotes-query/src/components/AnecdoteForm.jsx
@@ -1,8 +1,10 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useNotificationDispatch } from '../NotificationContext'
 import { createAnecdote } from '../requests'
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient()
+  const dispatch = useNotificationDispatch()
 
   const newMutants = useMutation({
     mutationFn: createAnecdote,
@@ -15,10 +17,25 @@ const AnecdoteForm = () => {
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
+
+    if (content.length < 5) {
+      dispatch({
+        type: 'SHOW',
+        payload: 'Content length is too short.'
+      })
+      setTimeout(() => { dispatch({ type: 'HIDE' }) }, 3000)
+      return
+    }
+
     event.target.anecdote.value = ''
     console.log('new anecdote')
+    dispatch({
+      type: 'SHOW',
+      payload: 'New anecdote created.'
+    })
+    setTimeout(() => { dispatch({ type: 'HIDE' }) }, 3000)
     newMutants.mutate({ content: content, votes: 0 })
-}
+  }
 
   return (
     <div>
